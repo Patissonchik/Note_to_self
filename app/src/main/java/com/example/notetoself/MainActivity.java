feb4223f8823f2852f1e1feb24afde848ae76ea3
Patissonchik <bokov.es@mail.ru>
2020-03-12 17:31:38 +0500

    3/4. Posle etogo ostalos' tol'ko zapilit' sohranenie

--- app/src/main/java/com/example/notetoself/MainActivity.java
+++ app/src/main/java/com/example/notetoself/MainActivity.java
@@ -7,6 +7,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
@@ -17,9 +21,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Note> listNote = new ArrayList<>();
-
+    RecyclerView recyclerView;
+    NoteAdapter adapter;
     public void createNewNote(Note note){
         listNote.add(note);
+        adapter.notifyDataSetChanged();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +35,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-//        final Button button = (Button)findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DialogShowNote dialog = new DialogShowNote();
-//                dialog.sendNoteSelected(tempNote);
-//                dialog.show(getSupportFragmentManager(), "123");
-//            }
-//        });
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show(getSupportFragmentManager(), "");
             }
         });
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        adapter = new NoteAdapter(this, listNote);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -69,5 +73,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void showNote(int index) {
+        DialogShowNote dialog = new DialogShowNote();
+        dialog.sendNoteSelected(listNote.get(index));
+        dialog.show(getSupportFragmentManager(), "");
     }
 }
